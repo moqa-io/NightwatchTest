6a1cccc9b5d6d490ff3d1c4a951674666827ce55
Mirza Music <nino.mirza@gmail.com>
2020-10-29 12:03:34 +0100

    init

---
+++ tests/nightwatchTest.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'Should display an error message when you enter a wrong login information' : function (client) {
+            client
+            .url ('https://www.netflix.com/')
+            .waitForElementVisible('body', 1000)
+            .useXpath().click('//a[@href="/login"]')
+        .useCss().setValue('#id_userLoginId', 'test@gmail.com')
+        .setValue('#id_password' ,'1234')
+        .useXpath().click('//button[text()="Sign In"]')
+        .useXpath().assert.visible('//div[@class="ui-message-contents"]')
+        .end();
+
+    }
+};
